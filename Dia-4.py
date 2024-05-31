18b8d5fd253ec1fc40f0e81d85a93bd5b7c13ed1
Patrick Coelho <100944831+riquelmept@users.noreply.github.com>
2024-05-31 19:48:24 -0300

    Update Dia-4.py

--- Dia-4.py
+++ Dia-4.py
@@ -1,5 +1,7 @@
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+from sklearn.neighbors import NearestNeighbors
+from random import randint
 
 import os
 for dirname, _, filenames in os.walk('C:/Users/patri/OneDrive/Documentos/Códigos/Python/7-days-of-code-CD/Data/ml-100k'):
@@ -9,201 +11,142 @@ for dirname, _, filenames in os.walk('C:/Users/patri/OneDrive/Documentos/Código
 # You can write up to 20GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using "Save & Run All" 
 # You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current ses
 
-data_filmes = pd.read_csv('C:/Users/patri/OneDrive/Documentos/Códigos/Python/7-days-of-code-CD/Data/ml-100k/u.item', sep='|', encoding='latin-1', header=None, index_col=False)
+data_filmes = pd.read_csv('C:/Users/patri/OneDrive/Documentos/Códigos/Python/7-days-of-code-CD/Data/ml-100k/u.item', sep='|', encoding='latin-1', header=None)
 data_filmes.head()
 
-data_filmes.columns = ['movie_id', 'movie_title', 'release_date', 'video_release_date', 'IMDb_URL', 'unknown',
-                 'Action', 'Adventure', 'Animation', 'Children', 'Comedy', 'Crime', 'Documentary', 
-                 'Drama', 'Fantasy', 'FilmNoir', 'Horror', 'Musical', 'Mystery', 'Romance', 'SciFi', 
-                 'Thriller', 'War', 'Western']
-data_filmes.head
+data_notas = pd.read_csv('C:/Users/patri/OneDrive/Documentos/Códigos/Python/7-days-of-code-CD/Data/ml-100k/u.data', sep='\t', header = None)
+data_notas.head()
 
-data_filmes.shape
+#Renomeando e filtrando as colunas
 
-data_filmes.info
+data_notas.rename(columns={0 :'user_id', 1:'movie_id', 2:'rating', 3:'timestamp'}, inplace=True)
+data_notas = data_notas[['user_id', 'movie_id', 'rating']]
+data_notas
 
-data_filmes.video_release_date.unique()
+#Renomeando e filtrando as colunas da base de filmes.
 
-data_filmes.drop(columns= ['video_release_date'], inplace = True)
-data_filmes.head()
+data_filmes.rename(columns={0: 'movie_id', 1: 'movie_title', 2: 'release_date', 3: 'video_release_date', 4: 'imdb_url', 5:
+'unknown', 6: 'action', 7: 'adventure', 8: 'animation', 9: 'childrens', 10: 'comedy', 11: 'crime', 12:
+'documentary', 13: 'drama', 14: 'fantasy', 15: 'film_noir', 16: 'horror', 17: 'musical', 18: 'mystery', 19:
+'romance', 20: 'scifi', 21: 'thriller', 22: 'war', 23: 'western'}, inplace=True)
+data_filmes = data_filmes[['movie_id','movie_title']]
+data_filmes
 
-notas = pd.read_csv('C:/Users/patri/OneDrive/Documentos/Códigos/Python/7-days-of-code-CD/Data/ml-100k/u.data', encoding = 'latin-1', sep = "\t", header = None, index_col = False)
-notas.columns = ['user_id', 'item_id', 'rating', 'timestamp']
-notas.head()
+# Juntando as tabelas. Agora mostra o usuário, o que ele viu e qual a nota ele deu
 
-notas.shape
+df_ttd = pd.merge(data_notas, data_filmes, on='movie_id', how='left')
+df_ttd = df_ttd[['user_id', 'movie_id', 'movie_title', 'rating']]
+df_ttd
 
-notas.info()
+# Tramsformando no tipo json para poder subir no firebase
 
-notas.describe()
-# Escolhendo o ID dos filmes como index
-data_filmes = data_filmes.set_index('movie_id')
+df_ttd.to_json('filmes_notas.json')
 
-# Selecionando um filme através do seu ID (escolhendo pelo horário que estou digitando esta linha)
-data_filmes.loc[1143]
+# Subindo minhas notas para os filmes
 
-totally_of_votes = notas['item_id'].value_counts()
-totally_of_votes.head()
+contador = 0
+nome = input('Digite seu nome: ')
+while contador < 5:
+    sorteio = randint(0, len(data_filmes) - 1)
+    dica = data_filmes['movie_title'][sorteio]
+    nota = int(input(f"Nota para {dica}"))
+    if nota > 0 :
+        contador += 1
+        usuario_nome = {'user_id': nome, 'movie_title' : dica, 'rating' : nota}
+        df_ttd = pd.concat([df_ttd, pd.DataFrame([usuario_nome])], ignore_index=True)
+print('FIM')
 
-data_filmes['Total_de_Votos'] = totally_of_votes
-data_filmes.head()
+# Pivot deixa em um formato que produz a intersecção do usuário e  filme, mostrando a nota
 
-data_filmes.sort_values('Total_de_Votos', ascending = False)
+# Fillna preenche valores nulos. Importante na hora da fórmula
 
-notas_medias = notas.groupby("item_id").mean()['rating']
-notas_medias.head()
+#não é possível usar apenas o pivot porque o indice é duplicado
 
-data_filmes['Nota_Media'] = notas_medias
-data_filmes.head()
 
-data_filmes.sort_values("Nota_Media", ascending= False).head(15)
+recomendacao = df_ttd.pivot_table(index='user_id', columns='movie_title', values='rating').fillna(0)
 
-data_filmes.query('Total_de_Votos >= 10').sort_values('Nota_Media', ascending= False)
+# Criando o modelo e alimentando com as informações
 
-data_filmes.Total_de_Votos.min()
+modelo = NearestNeighbors(metric='cosine')
+modelo.fit(recomendacao)
 
-data_filmes.Total_de_Votos.max()
+# Executando o modelo de recomendação e dividindo em distância (indíce de proximidade) e vizinhos (os que são próximos )
 
-filmes_com_mais_de_100_votos = data_filmes.query('Total_de_Votos >= 50')
-filmes_com_mais_de_100_votos.sort_values('Nota_Media', ascending = False)
+distancia, vizinhos = modelo.kneighbors(recomendacao.loc[nome].values.reshape(1,-1), n_neighbors=6)
 
-filmes_com_mais_de_100_votos.sample(10)
+vizinhos
 
-assistido = [20, 186, 303, 168, 255, 658, 636, 825, 82, 491]
-filmes_com_mais_de_100_votos.query('Action == 1 and Drama == 1')
+distancia
 
-acao_drama = filmes_com_mais_de_100_votos.query('Action == 1 and Drama == 1')
-acao_drama.sort_values('Nota_Media', ascending = False).head(5)
+# Mostrando os usuários parecidos e suas distâncias
 
-# Deste resultado, eu exclui os filmes que já assisti:
-acao_drama.drop(assistido, errors = 'ignore').sort_values('Nota_Media', ascending = False).head(5)
-
-usuario_50 = notas.query('user_id == 50')
-usuario_50[['item_id', 'rating']].set_index('item_id')
-
-def historico_usuario(usuario):
-    historico_usuario = notas.query('user_id == %d' % usuario)
-    historico_usuario = historico_usuario[['item_id', 'rating']].set_index('item_id')
-    return historico_usuario
-
-historico_usuario(50)
-
-historico_usuario(75)
-
-user50 = historico_usuario(43)
-user75 = historico_usuario(62)
-
-user50.join(user75, lsuffix='_do_user', rsuffix='_comparacao').dropna()
-
-diferenca = user50.join(user75, lsuffix='_do_user',rsuffix='_comparacao').dropna()
-np.linalg.norm(diferenca['rating_do_user'] - diferenca['rating_comparacao'])
-
-def distancia_entre_perfis(user_id1, user_id2):
-    notas1 = historico_usuario(user_id1)
-    notas2 = historico_usuario(user_id2)
-    diferenca = notas1.join(notas2, lsuffix = '_do_user', rsuffix = '_comparacao').dropna()
-    distancia = np.linalg.norm(diferenca['rating_do_user'] - diferenca['rating_comparacao'])
-    return [user_id1, user_id2, distancia]
-
-distancia_entre_perfis(50, 75)
-
-notas.user_id.unique()
-
-print('O dataset possui %d usuarios.' % len(notas.user_id.unique()))
-
-usuario1 = 50
-
-distancias = []
-for usuario in notas['user_id'].unique():
-    calculo = distancia_entre_perfis(usuario1, usuario)
-    distancias.append(calculo)
-
-distancias[:5]
-
-# Função para calcular a distância entre 1 usuário específico e os demais:
-
-def distancia_entre_usuarios(usuario_1):
-    todos_os_usuarios = notas['user_id'].unique()
-    distancias = [distancia_entre_perfis(usuario_1, user_id) for user_id in todos_os_usuarios]
-    distancias = pd.DataFrame(distancias, columns = ['Usuario_1', 'Outro_user', 'Distancia'])
-    return distancias
-
-distancia_entre_usuarios(50).head()
-
-# Ordenando o Dataframe pela Distância:
-
-def mais_proximos_de(usuario_1):
-    distancias = distancia_entre_usuarios(usuario_1)
-    distancias = distancias.sort_values('Distancia')
-    distancias = distancias.set_index('Outro_user').drop(usuario_1)
-    return distancias
-
-mais_proximos_de(50)
-
-mais_proximos_de(50).head(20)
-
-def distancia_entre_perfis(user_id1, user_id2, minimo = 5):
-    notas1 = historico_usuario(user_id1)
-    notas2 = historico_usuario(user_id2)
-    diferenca = notas1.join(notas2, lsuffix = '_do_user', rsuffix = '_comparacao').dropna()
-    
-    if(len(diferenca) < minimo):
-        return None
-    
-    distancia = np.linalg.norm(diferenca['rating_do_user'] - diferenca['rating_comparacao'])
-    return [user_id1, user_id2, distancia]
-
-def distancia_entre_usuarios(user_1, numero_maximo_de_analise = None):
-    todos_os_usuarios = notas['user_id'].unique()
-    
-    if numero_maximo_de_analise:
-        todos_os_usuarios = todos_os_usuarios[:numero_maximo_de_analise]
+for i in range(0, len(distancia.flatten())):
+    if i == 0:
+        print(f'Usuário: {nome}\n')
+    else:
+        print(f'Vizinho: {recomendacao.index[vizinhos.flatten()[i]]} \nDistância de: {distancia.flatten()[i]}')
         
-    distancias = [distancia_entre_perfis(user_1, user_id) for user_id in todos_os_usuarios]
-    distancias = list(filter(None, distancias))
-    distancias = pd.DataFrame(distancias, columns = ['Usuario_1', 'Outro_user', 'Distancia'])
-    return distancias
+        # Dataset do usuário com suas notas
 
-def mais_proximos_de(user_1, quantidade_user_proximos = 10, numero_maximo_de_analise = None):
-    distancias = distancia_entre_usuarios(user_1, numero_maximo_de_analise = numero_maximo_de_analise)
-    distancias = distancias.sort_values('Distancia')
-    distancias = distancias.set_index('Outro_user').drop(user_1)
-    return distancias.head(quantidade_user_proximos)
+ds_usuario = recomendacao.loc[nome].to_frame()
+ds_usuario
 
-mais_proximos_de(196, numero_maximo_de_analise = 50)
+# Dataset do vizinho mais próximo com suas notas
 
-def sugestoes(user1, quantidade_user_proximos = 10, numero_maximo_de_analise = None):
-    notas_user1 = historico_usuario(user1)
-    historico_filmes = notas_user1.index
-    similares = mais_proximos_de(user1, quantidade_user_proximos = quantidade_user_proximos, 
-                                 numero_maximo_de_analise = numero_maximo_de_analise)
-    usuarios_similares = similares.index
-    notas_dos_similares = notas.set_index('user_id').loc[usuarios_similares]
-    recomendacoes = notas_dos_similares.groupby('item_id').mean()[['rating']]
-    recomendacoes = recomendacoes.sort_values('rating', ascending = False)
-    return recomendacoes.join(data_filmes).head()
+vizinho_prox = recomendacao.index[vizinhos.flatten()[1]]
+ds_vizinho = recomendacao.loc[vizinho_prox].to_frame()
+ds_vizinho
 
-sugestoes(196, quantidade_user_proximos = 2, numero_maximo_de_analise = 50)
+# Junção dos datasets e mostrando ordem decrescente
 
-sugestoes(196, numero_maximo_de_analise = 50)
+ds_recomendacao = pd.merge(ds_usuario, ds_vizinho, on='movie_title').sort_values(by=vizinho_prox, ascending=False)
 
-sugestoes(196)
+# Filtrando com os que o usuário que receberá a informação não tenha visto
 
-data_filmes.sample(11)
+ds_recomendacao = ds_recomendacao.query(f'{nome} == 0 and {recomendacao.index[vizinhos.flatten()[i]]} > 0')
 
-import random
-notas_novo_user = [random.randint(1,5) for x in range(11)]
-notas_novo_user
+ds_recomendacao[:5]
 
-def novo_usuario(seus_filmes):
-    novo_usuario = notas['user_id'].max()+1
-    notas_do_usuario_novo = pd.DataFrame(seus_filmes, columns = ['item_id', 'rating'])
-    notas_do_usuario_novo['user_id'] = novo_usuario
-    return pd.concat([notas, notas_do_usuario_novo])
+# Dataset do usuário
 
-notas = novo_usuario([[723, 3], [1094, 3], [652, 1], [1522, 5], [1243, 2], [1456, 1], [1086, 5], [1034, 2], [703, 2], [90, 3], [217, 2]])
-notas.tail(12)
+ds_usuario = recomendacao.loc[nome].to_frame()
+ds_usuario
 
-# Para o novo usuário temos as seguintes recomendações:
+# Criando um novo dataset com o nome do filme e a nota dada pelo usuário, sem o indíce 
+vizinhanca = ds_usuario
+vizinhanca_df = vizinhanca.reset_index()
+vizinhanca_df
 
-sugestoes(944)
+# Acrescentando os vizinhos ao dataset 
+
+for i in range(1, 6):
+    vizinho_prox = recomendacao.index[vizinhos.flatten()[i]]
+    ds_vizinho = recomendacao.loc[vizinho_prox].to_frame()
+    vizinhanca = pd.merge(vizinhanca, ds_vizinho[vizinho_prox], on='movie_title')
+    
+#Resetando o index
+
+vizinhanca = vizinhanca.reset_index()
+
+# estabelecendo os vizinhos
+
+pos1 = recomendacao.index[vizinhos.flatten()[1]]
+pos2 = recomendacao.index[vizinhos.flatten()[2]]
+pos3 = recomendacao.index[vizinhos.flatten()[3]]
+pos4 = recomendacao.index[vizinhos.flatten()[4]]
+pos5 = recomendacao.index[vizinhos.flatten()[5]]
+
+# Acrescento os vizinhos e as notas dadas por eles. No final, faço uma coluna soma e indico os filmes favoritos do grupo
+
+vizinhanca['SOMA'] = vizinhanca[pos1] + vizinhanca[pos2] + vizinhanca[pos3] + vizinhanca[pos4] + vizinhanca[pos5]
+indicacao = vizinhanca.sort_values(by='SOMA', ascending=False)
+indicacao = indicacao.query(f'{nome} == 0')
+list(indicacao['movie_title'][:5])
+
+indicacao
+
+#exportando o modelo 
+
+import pickle
+filename = 'modelo_ptr.pk1'
+pickle.dump(modelo, open(filename, 'wb'))
